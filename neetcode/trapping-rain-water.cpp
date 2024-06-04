5aa55f2cbf4c75a9020c25d290c91ecf57bf03a1
sreeharij <sreeharij2003@gmail.com>
2024-06-04 12:56:51 +0530

    add O(1) space complexity sol

--- neetcode/trapping-rain-water.cpp
+++ neetcode/trapping-rain-water.cpp
@@ -1,3 +1,4 @@
+//O(n) time and O(n) space solution
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -19,6 +20,35 @@ public:
             int water = min(left_boundary[i],right_boundary[i]) - height[i];
             answer +=  water > 0 ? water : 0;
         }
+        return answer;
+    }
+};
+
+//O(n) time and O(1) space solution
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int length = height.size();
+        if(length < 3)return 0;
+        int answer = 0;
+        int left_ptr = 1,right_ptr = length - 2;
+        int left_max = height[0];
+        int right_max = height[length-1];
+        int water;
+        while(left_ptr <= right_ptr){
+            if(left_max <= right_max){
+                water = left_max - height[left_ptr];
+                left_max = max(left_max,height[left_ptr]);
+                left_ptr++;
+            }
+            else{
+                water = right_max - height[right_ptr];
+                right_max = max(right_max,height[right_ptr]);
+                right_ptr--;
+            }
+            answer += water > 0 ? water : 0;
+        }
+
         return answer;
     }
 };
