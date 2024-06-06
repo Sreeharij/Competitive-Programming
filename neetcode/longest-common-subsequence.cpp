192e9bbc8df95347471426075801c47373e11352
sreeharij <sreeharij2003@gmail.com>
2024-06-06 15:07:54 +0530

    add space optimised solution

--- neetcode/longest-common-subsequence.cpp
+++ neetcode/longest-common-subsequence.cpp
@@ -1,3 +1,4 @@
+//o(m*n) time and O(m*n) space
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2){
@@ -16,5 +17,40 @@ public:
         }
 
         return dp[n][m];
+    }
+};
+
+
+//Space optimised version. O(m*n)time and O(min(m,n)) space
+class Solution {
+public:
+    void swap(string& a,string& b){
+        string temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int longestCommonSubsequence(string text1, string text2){
+        if(text1.size() < text2.size()){
+            swap(text1,text2);
+        }
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<int> prev(m+1,0);
+        vector<int> dp(m+1,0);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[j] = prev[j-1] + 1;
+                }
+                else{
+                    dp[j] = max(dp[j-1],prev[j]);
+                }
+            }
+            prev = dp;
+        }
+        
+        return dp[m];
     }
 };
