9233e1df7584c53ef3d223402efa38ec91beed3a
sreeharij <sreeharij2003@gmail.com>
2024-06-07 13:28:57 +0530

    add space optimisation for triangle question

--- leetcode/triangle.cpp
+++ leetcode/triangle.cpp
@@ -18,3 +18,27 @@ public:
         return answer;
     }
 };
+
+//Add space optimised solution O(n) space
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int length = triangle.size();
+        vector<int> prev(length,INT_MAX);
+        vector<int> dp(length,INT_MAX);
+        prev[0] = triangle[0][0];
+        for(int i=0;i<length-1;i++){
+            for(int j=0;j<=i;j++){
+                dp[j] = min(dp[j],prev[j] + triangle[i+1][j]);
+                dp[j+1] = min(dp[j+1],prev[j] + triangle[i+1][j+1]);
+            }
+            prev = dp;
+            fill(dp.begin(),dp.end(),INT_MAX);
+        }
+        int answer = INT_MAX;
+        for(int i=0;i<length;i++){
+            answer = min(answer,prev[i]);
+        }
+        return answer;
+    }
+};
