7c69595cbe720aa4300d844a1ae0d08cf94d8e4a
sreeharij <sreeharij2003@gmail.com>
2024-06-07 11:02:29 +0530

    more optimised sol longest pallindromic substring

--- neetcode/longest-palindromic-substring.cpp
+++ neetcode/longest-palindromic-substring.cpp
@@ -7,22 +7,22 @@ public:
         for(int i=0;i<length-1;i++){       //LOOT FINISHES at i < length - 1 since this is correct for both EVEN and ODD length pallindros
             int l = i,r = i;
             while(l >= 0 && r <= length-1 && s[l] == s[r]){
-                if(r - l + 1 > size){
-                    size = r - l + 1;
-                    answer = s.substr(l,size);
-                }
                 l--;
                 r++;
+            }
+            if(r - l - 1 > size){   // r-l+1 is size of substring had r and l been both part of the substring. so do r-l+1-2 = r-l-1
+                size = r - l - 1;
+                answer = s.substr(l+1,size);
             }
 
             l = i,r=i+1;
             while(l >= 0 && r <= length-1 && s[l] == s[r]){
-                if(r - l + 1 > size){
-                    size = r - l + 1;
-                    answer = s.substr(l,size);
-                }
                 l--;
                 r++;
+            }
+            if(r - l - 1 > size){
+                size = r - l - 1;
+                answer = s.substr(l+1,size);
             }
         }
         return answer;
