0b680e2796530b7c272a0135c71b1fbb83d6b07d
sreeharij <sreeharij2003@gmail.com>
2024-06-09 16:16:38 +0530

    add heapsort solution to sort an array

--- leetcode/sort-an-array.cpp
+++ leetcode/sort-an-array.cpp
@@ -1,3 +1,4 @@
+//Sorting an array using merge sort
 class Solution {
 public:
     void merge(vector<int>& nums,vector<int>& left_arr,vector<int>& right_arr,int left,int mid,int right){
@@ -41,6 +42,55 @@ public:
         vector<int> right_arr(n,0);
         mergeSort(nums,left_arr,right_arr,0,n-1);
 
+        return nums;
+    }
+};
+
+//Sorting an array using heapsort
+class Solution {
+public:
+    void swap(int& a,int& b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void heapify(vector<int>& nums,int i,int n){
+        int left_child,right_child,largest;
+        while(true){
+            left_child = 2*i+1;
+            right_child = 2*i+2;
+            largest = i;
+            if(left_child < n && nums[left_child] > nums[largest]){
+                largest = left_child;
+            }
+            if(right_child < n && nums[right_child] > nums[largest]){
+                largest = right_child;
+            }
+            if(i == largest)return;
+            swap(nums[i],nums[largest]);
+            i = largest;
+        }
+    }
+
+    void build_max_heap(vector<int>& nums,int n){
+        int start_idx = n/2-1;
+        for(int i=start_idx;i>=0;i--){
+            heapify(nums,i,n);
+        }
+    }
+
+    void heapsort(vector<int>& nums,int n){
+        build_max_heap(nums,n);
+
+        for(int i=n-1;i>0;i--){
+            swap(nums[0],nums[i]);
+            heapify(nums,0,i);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        heapsort(nums,nums.size());
         return nums;
     }
 };
