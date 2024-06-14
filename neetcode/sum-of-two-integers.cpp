3c4faed1978f39d891e256b5b050cb81bd43e4ab
sreeharij <sreeharij2003@gmail.com>
2024-06-14 22:57:37 +0530

    add more compact solution to sum of two integers

--- neetcode/sum-of-two-integers.cpp
+++ neetcode/sum-of-two-integers.cpp
@@ -14,3 +14,16 @@ public:
         return answer;
     }
 };
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry;
+        while(b != 0){
+            carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+};
