8e30f65eb851df0b35d51dda205d4a06eb9d827f
sreeharij <sreeharij2003@gmail.com>
2024-06-14 20:39:50 +0530

    optimised sol for largest rectangle in histogram

--- neetcode/largest-rectangle-in-histogram.cpp
+++ neetcode/largest-rectangle-in-histogram.cpp
@@ -1,3 +1,4 @@
+//O(n) time and O(3n) space (stack and 2 vector arrays)
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
@@ -19,6 +20,34 @@ public:
         int answer = INT_MIN;
         for(int i=0;i<length;i++){
             answer = max((next_smaller[i] - previous_smaller[i] - 1)*heights[i],answer);
+        }
+        return answer;
+    }
+};
+
+
+//O(n) time and O(n) space for stack
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int length = heights.size();
+        stack<int> stk;
+        stk.push(0);
+        int answer = INT_MIN;
+        int height,width;
+        for(int i=1;i<=length;i++){
+            while(!stk.empty() && (i == length || heights[i] < heights[stk.top()])){
+                height = heights[stk.top()];
+                stk.pop();
+                if(stk.empty()){
+                    width = i;
+                }
+                else{
+                    width = i - stk.top() - 1;
+                }
+                answer = max(answer,width*height);
+            }
+            stk.push(i);
         }
         return answer;
     }
