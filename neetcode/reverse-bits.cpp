c7207e91cf1be4c2136687fc18037007a143f563
sreeharij <sreeharij2003@gmail.com>
2024-06-15 00:01:06 +0530

    add sol to reverse bits

--- neetcode/reverse-bits.cpp
+++ neetcode/reverse-bits.cpp
@@ -9,3 +9,16 @@ public:
         return answer;
     }
 };
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t answer= 0;
+        for(int i=0;i<32;i++){
+            answer = answer << 1;
+            answer = answer | (1 & n);
+            n = n >> 1;
+        }
+        return answer;
+    }
+};
