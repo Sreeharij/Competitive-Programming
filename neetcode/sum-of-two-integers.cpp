7f37e1763d6f0830fc1ff483ec24cf685f322a65
sreeharij <sreeharij2003@gmail.com>
2024-06-14 22:56:36 +0530

    add sol to sum of two integers

---
+++ neetcode/sum-of-two-integers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int a_bit,b_bit;
+        int answer = 0;
+        int carry = 0;
+        for(int i=0;i<32;i++){
+            a_bit = (a & 1 << i);
+            b_bit = (b & 1 << i);
+            answer = answer | (a_bit ^ b_bit)^carry;
+            carry = (a_bit & carry) | (b_bit & carry) | (a_bit & b_bit);
+            carry = carry << 1;
+        }
+        return answer;
+    }
+};
