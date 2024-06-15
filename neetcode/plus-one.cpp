4c8457e72a115e15d4c2d4457c01c4df0ccf287c
sreeharij <sreeharij2003@gmail.com>
2024-06-15 15:14:06 +0530

    add sol to plus one

---
+++ neetcode/plus-one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int length = digits.size();
+        int non_9_idx = -1;
+        for(int i=length-1;i>=0;i--){
+            if(digits[i] != 9){
+                non_9_idx = i;
+                break;
+            }
+        }      
+        if(non_9_idx == -1){
+            vector<int> answer(length+1,0);
+            answer[0] = 1;
+            return answer;
+        }
+        else{
+            vector<int> answer(length,0);
+            for(int i=0;i<non_9_idx;i++){
+                answer[i] = digits[i];
+            }
+            answer[non_9_idx] = digits[non_9_idx]+1;
+            return answer;
+        }
+    }
+};
