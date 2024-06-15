14d089183d872dc6a979090324f3ca182d6073ee
sreeharij <sreeharij2003@gmail.com>
2024-06-16 01:03:31 +0530

    add sol to plus one

--- neetcode/plus-one.cpp
+++ neetcode/plus-one.cpp
@@ -24,3 +24,21 @@ public:
         }
     }
 };
+
+//CLEANER CODE
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.push_back(0);
+        digits[0] = 1;
+        return digits;
+    }
+};
