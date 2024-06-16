84d41ec06da4911a7e2ef906724cddffe19c4166
sreeharij <sreeharij2003@gmail.com>
2024-06-16 14:01:26 +0530

    optimised solution for hand of straights

--- neetcode/hand-of-straights.cpp
+++ neetcode/hand-of-straights.cpp
@@ -1,3 +1,4 @@
+//O(nlogn) time solution
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
@@ -16,6 +17,29 @@ public:
                 if(mp[number+i] == 0)mp.erase(number+i);   
             }
             groups--;
+        }
+        return true;
+    }
+};
+
+//O(n) time solution
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int length = hand.size();
+        if(length%groupSize != 0) return false;
+        sort(hand.begin(),hand.end());
+        unordered_map<int,int> freq;
+
+        for(int number: hand)freq[number]++;
+
+        for(int number: hand){
+            if(freq[number] == 0)continue;
+
+            for(int i=0;i<groupSize;i++){
+                if(freq[number+i] == 0)return false;
+                freq[number+i]--;
+            }
         }
         return true;
     }
