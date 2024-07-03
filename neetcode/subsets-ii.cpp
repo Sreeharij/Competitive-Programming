59a961d1c14d788da9cc3979b84ae1dbba4824e2
sreeharij <sreeharij2003@gmail.com>
2024-07-03 12:11:38 +0530

    iterative solution to get subsets with duplicates

--- neetcode/subsets-ii.cpp
+++ neetcode/subsets-ii.cpp
@@ -35,3 +35,31 @@ public:
         return answer;
     }
 };
+
+
+//Iterative solution below
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> answer;
+        sort(nums.begin(),nums.end());
+        answer.push_back({});
+        int length = nums.size();
+        int left = 0,right = 0;
+        for(int i=0;i<length;i++){
+            left = 0;
+
+            if(i > 0 && nums[i] == nums[i-1]){
+                left = right;
+            }
+
+            right = answer.size();
+            for(int j=left;j<right;j++){
+                vector<int> new_subset = answer[j];
+                new_subset.push_back(nums[i]);
+                answer.push_back(new_subset);
+            }
+        }
+        return answer;
+    }
+};
