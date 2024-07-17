9c828cc44ef7721aab979d4d17b2912876214a15
sreeharij <sreeharij2003@gmail.com>
2024-07-17 11:07:48 +0530

    add sol to happy number

--- neetcode/happy-number.cpp
+++ neetcode/happy-number.cpp
@@ -1,3 +1,4 @@
+//O(n) time and O(n) space where n is the length of loop + length of elements before loop
 class Solution {
 public:
     int comptue_result(int n){
@@ -19,5 +20,36 @@ public:
             if(n == 1)return true;
             if(st.count(n)) return false;
         }
+    }
+};
+
+
+//O(1) space complexity
+class Solution {
+public:
+    int comptue_result(int n){
+        int digit;
+        int result = 0;
+        while(n > 0){
+            digit = n%10;
+            n = n/10;
+            result += digit*digit;
+        }
+        return result;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = comptue_result(n);
+
+        while(slow != fast){           
+            slow = comptue_result(slow);
+            fast = comptue_result(comptue_result(fast));
+        } 
+
+        if(fast == 1){
+            return true;
+        }
+        return false;
     }
 };
