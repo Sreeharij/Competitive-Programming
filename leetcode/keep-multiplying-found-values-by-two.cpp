bbe7f3ecf166c5f874f8894e167d3535fa36553a
sreeharij <sreeharij2003@gmail.com>
2025-11-19 21:07:16 +0530

    add O(n) time O(1) space sol

--- leetcode/keep-multiplying-found-values-by-two.cpp
+++ leetcode/keep-multiplying-found-values-by-two.cpp
@@ -13,3 +13,31 @@ public:
         return original;
     }
 };
+
+
+//below contains efficient solution
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int answer = 0;
+        int remaining;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] % original != 0)continue;
+            remaining = nums[i]/original;
+            if((remaining & (remaining-1)) == 0){
+                answer = answer | remaining;
+            }
+        }
+        int highest_2 = 1;
+        while(1){
+            if((answer & 1) != 0){
+                highest_2 *= 2;
+                answer >>= 1;
+            }
+            else return highest_2 * original;
+            if(answer <= 0) return highest_2 * original;
+        }
+        return original;
+
+    }
+};
